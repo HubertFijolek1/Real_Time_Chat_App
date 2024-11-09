2cdfe08508355758bb77bb7c567b4319fd382c52
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-09 10:51:08 +0100

    Implement chat message storage with Redis

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from . import redis_client
 
 app = FastAPI()
 
@@ -14,6 +15,12 @@ def read_root():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    # Send last 50 messages
+    messages = redis_client.lrange("chat_messages", -50, -1)
+    for message in messages:
+        await websocket.send_text(message)
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        redis_client.rpush("chat_messages", data)
+        redis_client.ltrim("chat_messages", -50, -1)
+        await websocket.send_text(data)
