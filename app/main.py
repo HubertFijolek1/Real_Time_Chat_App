693046889eeea9401cbe336cec64f688a12a2e6a
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-09 10:24:38 +0100

    Serve frontend files with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+app.mount("/static", StaticFiles(directory="../frontend"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Welcome to the Real-Time Chat App"}
+    with open("../frontend/index.html") as f:
+        return f.read()
