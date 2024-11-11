6e72dac0449bd2cf7dba48c7173331696e54980f
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-11 11:34:46 +0100

    Implement Password Hashing

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,3 +15,9 @@ class User(Base):
     # Relationships (to be defined later)
     messages = relationship("Message", back_populates="user")
     memberships = relationship("Membership", back_populates="user")
+
+    def set_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
