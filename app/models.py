f1d8eee886787ed4cffd29da46096cbf780df337
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-11 11:34:03 +0100

    Create User Model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from .database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+    # Relationships (to be defined later)
+    messages = relationship("Message", back_populates="user")
+    memberships = relationship("Membership", back_populates="user")
